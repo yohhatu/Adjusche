1db0a203616860e75f832e456d58b3bf19802953
seit <uent192837465@gmail.com>
2012-01-14 10:46:36 +0900

    雛形cimmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Adjusche::Application.config.secret_token = '4b1277c2ec0ef6f8f44f1e7ce5115c2e440cfe875d0ac60ae43ee3135c446fa151b5fb5c5baba8db38d6229c7bf06ccb88948380742a03563436ffc5533b611f'
