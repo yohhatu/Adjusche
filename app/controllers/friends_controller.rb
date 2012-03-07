15cc077f7fe83a989a5e29b64d96b9e8fa070f7f
seit <uent192837465@gmail.com>
2012-03-07 23:27:25 +0900

    友達検索画面にAjax追加

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,8 +1,22 @@
 class FriendsController < ApplicationController
+  $event_id
   def index
-   @event = Event.find(params[:id])
+   $event_id = params[:id]
+   @event = Event.find($event_id)
+
+   @users = User.find_by_user_name('seit')
   end
 
   def search
+    #@event = Event.find($event_id)
+    @s_name = params[:search_name]
+
+    # ユーーザー検索
+    @users = User.find_by_user_name(@s_name)
+
+    respond_to do |format|
+      format.js
+    end
+    #render :index
   end
 end
