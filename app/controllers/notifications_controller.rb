4a077ed22b63de8ae607e799693e9d26fa5bed3f
seit <uent192837465@gmail.com>
2012-05-06 23:51:29 +0900

    notificationコントローラ実装

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,2 +1,13 @@
 class NotificationsController < ApplicationController
+  def index 
+    @user = session[:user_info].user_name
+    @notifications = Notification.where(:user_id => 1 , :read_flag => false)
+
+    @notifications.each do |notify|
+      if notify.read_flag == 0 then
+        #通知のカインドをキーに、メッセージをデータベースから取得してくる
+      end
+    end
+
+  end
 end
