4d2dae31a548689fef54f45396a9f4e9c2ffc326
seit <uent192837465@gmail.com>
2012-05-16 23:06:48 +0900

    エンコーディング対応

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8 -*-
 class FriendsController < ApplicationController
   $event_id
   def index
--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8 -*-
 class LoginsController < ApplicationController
   def index
     render :layout => false
