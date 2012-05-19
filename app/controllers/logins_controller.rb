791cf090cda41fed2163ee170b492e0fc330825c
seit <uent192837465@gmail.com>
2012-05-19 18:34:38 +0900

    ログイン失敗時の不具合修正

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -7,19 +7,20 @@ class LoginsController < ApplicationController
   def login
     begin
       login_user = User.find_by_user_name_and_user_pass!( params[:post][:name] , params[:post][:pass] )
+      session[:user_info] = login_user
+      #@events = Event.where(:user_id => User.find_by_user_name(session[:user_info] )[:id])
+
+      @user_name = login_user.user_name
+
+      luid = login_user.id
+      @events = Event.where(:user_id => luid )
+
+      redirect_to :controller => "events", :action => "index"
+ 
     rescue ActiveRecord::RecordNotFound
       #redirect_to :controller => "logins", :action => "index"
       redirect_to :back
     end
 
-    session[:user_info] = login_user
-    #@events = Event.where(:user_id => User.find_by_user_name(session[:user_info] )[:id])
-
-    @user_name = login_user.user_name
-
-    luid = login_user.id
-    @events = Event.where(:user_id => luid )
-
-    redirect_to :controller => "events", :action => "index"
   end
 end
