094e96c5d8e12ddabce09f9f873c3b5e26974a16
seit <uent192837465@gmail.com>
2012-05-23 22:43:55 +0900

    通知に日付を表示

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,20 +2,20 @@
 class NotificationsController < ApplicationController
   def index 
     @user = session[:user_info]
-    @notifications = Notification.where(:user_id => @user.id , :read_flag => false)
-  
- 
+    @notifications = Notification.where(:user_id => @user.id , :read_flag => false) 
 
     @msgAry = Array.new()
     @notifications.each do |notify|
       if notify.read_flag == false then
+        h = Hash.new()
         #通知のカインドをキーに、メッセージをセレクトする
         case notify.notify_kind
         when "2" then
           @strUsrNm = User.find(@user.id)
           @strEvtNm = Event.find(notify.general_column2)
-          str = Message.plain("MSG_NOTIFY_INVITATION", [@strUsrNm.user_name,@strEvtNm.name])
-          @msgAry.push str
+          h.store('msg', Message.plain("MSG_NOTIFY_INVITATION", [@strUsrNm.user_name,@strEvtNm.name]))
+          h.store('date', notify.notify_date)
+          @msgAry.push h
         else
         end
       end
